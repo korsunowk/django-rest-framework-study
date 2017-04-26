cbf5674f2dc447f0458c3c994caf61f00c113128
Korsunow Konstantin <korsunowk@yandex.ua>
2017-04-26 13:50:32 +0300

    Add base-name to router urls

--- api/routers.py
+++ api/routers.py
@@ -4,6 +4,6 @@ from api import views
 
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'comments', views.CommentViewSet)
-router.register(r'subjects', views.SubjectViewSet)
+router.register(r'users', views.UserViewSet, base_name='user')
+router.register(r'comments', views.CommentViewSet, base_name='comment')
+router.register(r'subjects', views.SubjectViewSet, base_name='subject')

715acc326e97fc2c1421f90c3c4aac9cc76ddd85
Korsunow Konstantin <korsunowk@yandex.ua>
2017-05-11 16:25:03 +0300

    Upgrade timer with start/stop

--- static/js/components/block-counter.js
+++ static/js/components/block-counter.js
@@ -16,9 +16,16 @@ class CounterBlock extends React.Component {
             seconds: 0,
             milseconds: 0
         };
+        this.intervalObj = null;
+        this.interval = false;
+
         this.AddMilisecond = this.AddMilisecond.bind(this);
         this.startTime = this.startTime.bind(this);
+        this.stopTime = this.stopTime.bind(this);
+        this.handleTimeButtonClick = this.handleTimeButtonClick.bind(this);
+        this.clearTimer = this.clearTimer.bind(this);
     }
+
     AddMilisecond(){
         let milisec = this.state.milseconds + 2;
         let seconds = this.state.seconds;
@@ -44,19 +51,42 @@ class CounterBlock extends React.Component {
             milseconds: milisec
         })
     }
+
+    handleTimeButtonClick() {
+        if (this.interval)
+            this.stopTime();
+        else{
+            this.clearTimer();
+            this.startTime();
+        }
+    }
+    clearTimer() {
+        this.setState({
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+            milseconds: 0
+        })
+    }
     startTime() {
-        window.setInterval(
+        this.interval = true;
+        this.intervalObj = window.setInterval(
             this.AddMilisecond,
             1
         )
     }
+    stopTime() {
+        this.interval = false;
+        clearInterval(this.intervalObj);
+    }
+
     render () {
         let state = this.state;
         let timer = [state.hours, state.minutes, state.seconds, state.milseconds].join(':');
         return (
             <div className="block-counter">
                 <Counter counter={this.props.counter}/>
-                <TimeButton title="Time Button" className="time-button" onClick={this.startTime}/>
+                <TimeButton title="Time Button" className="time-button" onClick={this.handleTimeButtonClick}/>
                 <TimeDisplay title="Timer: " value={timer}/>
             </div>
         )

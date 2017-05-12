30dea507f75f251c3ef228ce90643afe77e7b869
Korsunow Konstantin <korsunowk@yandex.ua>
2017-05-12 13:27:50 +0300

    Create Watch with arrows (first step)

---
+++ static/js/components/watch/watch-arrow.js
@@ -0,0 +1,16 @@
+/**
+ * Created by base on 12.05.17.
+ */
+import React from 'react';
+
+class WatchArrow extends React.Component {
+    render () {
+        return (
+            <div className="watch__arrow">
+
+            </div>
+        )
+    }
+}
+
+export default WatchArrow;
---
+++ static/js/components/watch/watch.js
@@ -0,0 +1,17 @@
+/**
+ * Created by base on 12.05.17.
+ */
+import React from 'react';
+import WatchArrow from './watch-arrow'
+
+class Watch extends React.Component {
+    render() {
+        return (
+            <div className="watch">
+                <WatchArrow />
+            </div>
+        )
+    }
+}
+
+export default Watch;

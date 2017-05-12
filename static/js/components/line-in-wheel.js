2de0a1b7d447db9ccf4481b791bac73c2214d74a
Korsunow Konstantin <korsunowk@yandex.ua>
2017-05-12 11:33:03 +0300

    Upgrade OneLines, added new feature to stop/start single line

--- static/js/components/line-in-wheel.js
+++ static/js/components/line-in-wheel.js
@@ -6,34 +6,66 @@ import React from 'react';
 class OneWheelLine extends React.Component {
     constructor () {
         super();
+
+        this.intervalObj = null;
+        this.interval = false;
+
         this.state = {
-           value: false
+            value: 0,
+            rotate: 0,
+            stop: false
         };
 
-        this.startOneLine = this.startOneLine.bind(this);
+        this.toggleRotate = this.toggleRotate.bind(this);
         this.goSimpleHaos = this.goSimpleHaos.bind(this);
+        this.setRotate = this.setRotate.bind(this);
+    }
+    toggleRotate() {
+        if (this.interval){
+            this.interval = false;
+            clearInterval(this.intervalObj);
+        }
+        else{
+            this.interval = true;
+            this.goSimpleHaos();
+        }
     }
     goSimpleHaos() {
-        let add = this.state.value + 1;
+        if (!this.state.stop){
+            this.intervalObj = window.setInterval(this.setRotate, 100);
+            this.interval = true;
+        }
+    }
+    setRotate(){
+        let newValue = this.state.value + this.state.rotate;
         this.setState({
-            value: add
+            value: newValue
         })
     }
-    startOneLine(){
-
+    componentWillMount() {
+        if (this.props.stop)
+        {
+            this.setState({
+                stop: this.props.stop
+            })
+        }
+        this.setState({
+            rotate: this.props.rotate
+        });
     }
     render () {
-        let rotate = !this.state.value ? this.props.rotate: this.state.value;
-
         return (
                 <div
                     id={this.props.id}
                     className={this.props.className}
-                    style={{transform: "rotate(" + rotate + 'deg)'}}
-                    onClick={this.startOneLine}
+                    style={{transform: "rotate(" + this.state.value + 'deg)'}}
+                    onClick={this.toggleRotate}
                     key={this.props.key} >
                 </div>
         )
+    }
+    componentDidMount () {
+        this.goSimpleHaos();
     }
 }
 
--- static/js/components/simple-watch.js
+++ static/js/components/simple-watch.js
@@ -8,73 +8,47 @@ import OneWheelLine from './line-in-wheel'
 class SimpleWatch extends React.Component {
     constructor () {
         super();
-        this.interval = false;
-        this.intervalObj = null;
 
         this.state = {
-            red: 0,
-            orange: 0,
-            yellow: 0,
-            green: 0,
-            lightblue: 0,
-            blue: 0,
-            purple: 0
+            red: 1,
+            orange: 2,
+            yellow: 3,
+            green: 4,
+            lightblue: 5,
+            blue: 6,
+            purple: 7,
+            stop: false
         };
-
-        this.setHaos = this.setHaos.bind(this);
-        this.goHaos = this.goHaos.bind(this);
-        this.stopHaos = this.stopHaos.bind(this);
+        this.toogleHaos = this.toogleHaos.bind(this);
     }
-    setHaos(){
-        let newRed = this.state.red + 1;
-        let newOra = this.state.orange + 2;
-        let newYel = this.state.yellow + 3;
-        let newGre = this.state.green + 4;
-        let newLB = this.state.lightblue + 5;
-        let newB = this.state.blue + 6;
-        let newPurple = this.state.purple + 7;
 
-        this.setState({
-            red: newRed,
-            yellow: newYel,
-            green: newGre,
-            orange: newOra,
-            lightblue: newLB,
-            blue: newB,
-            purple: newPurple
-        })
-    }
-    goHaos() {
-        this.intervalObj = window.setInterval(this.setHaos, 1);
-        this.interval = true;
-    }
-    stopHaos(e){
-
+    toogleHaos(e) {
         if (e.target.className == 'simple-watch')
         {
-            if (this.interval){
-                this.interval = false;
-                clearInterval(this.intervalObj);
-            }
-            else{
-                this.interval = true;
-                this.goHaos();
-            }
+            let stop = !this.state.stop;
+
+            this.setState({
+                stop: stop
+            })
         }
     }
     render () {
         let colors = ["red", "yellow", "orange", "green", "lightblue", "blue", "purple"];
         let lines = [];
-
         for(let i=0; i< colors.length; i++){
             lines.push(
-                <OneWheelLine id={colors[i]} className={"simple-watch__" + colors[i]} rotate={this.state[colors[i]]} key={i} />
+                <OneWheelLine
+                    id={colors[i]}
+                    className={"simple-watch__" + colors[i]}
+                    rotate={this.state[colors[i]]}
+                    key={i}
+                    stop={this.state.stop}
+                />
             )
         }
-
         return (
             <div className="block-watch">
-                <div className="simple-watch" onClick={this.stopHaos}>
+                <div className="simple-watch" onClick={this.toogleHaos}>
                     <div className="simple-watch__center">
 
                     </div>
@@ -82,9 +56,6 @@ class SimpleWatch extends React.Component {
                 </div>
             </div>
         )
-    }
-    componentDidMount(){
-        this.goHaos();
     }
 }
 

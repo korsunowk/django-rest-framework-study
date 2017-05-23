5cb73001d24c0aab8bef20ee2c62acb78b744898
Korsunow Konstantin <korsunowk@yandex.ua>
2017-05-23 16:25:52 +0300

    Simple Watch with numbers and one arrow

--- static/js/components/canvas-watch/canvas-watch.js
+++ static/js/components/canvas-watch/canvas-watch.js
@@ -19,10 +19,19 @@ class CanvasWatch extends React.Component {
 
 		ctx.font = "35px Times New Roman";
 		ctx.fillText("12", centerX - 16, centerY - 120);
-		ctx.fillText("6", centerX - 6, centerY + 145);
-		ctx.fillText("9", centerX - 145, centerY);  // numbers on Watch
-		ctx.fillText("3", centerX + 125, centerY);
+		ctx.fillText("6", centerX - 9, centerY + 145);
+		ctx.fillText("9", centerX - 145, centerY + 12);  // numbers on Watch
+		ctx.fillText("3", centerX + 125, centerY + 12);
 
+		ctx.beginPath();
+		ctx.arc(centerX, centerY, 10, 0, 2 * Math.PI); // center of Watch
+		ctx.fill();
+
+		ctx.beginPath();
+		ctx.moveTo(centerX, centerY);
+		ctx.lineTo(centerX, centerY - 145);
+		ctx.lineWidth = "5px";
+		ctx.stroke();
 	}
 }
 
